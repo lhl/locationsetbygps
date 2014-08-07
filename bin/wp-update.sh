77efe6dc61ae3f0023b47816689f988a558ac5fb
lhl <lhl@randomfoo.net>
2014-08-07 07:54:08 -0400

    Tracking installed plugins

--- bin/wp-update.sh
+++ bin/wp-update.sh
@@ -6,3 +6,4 @@ cd /www/locationsetbygps/wp
 /usr/local/bin/wp core update
 /usr/local/bin/wp plugin update --all
 /usr/local/bin/wp theme update --all
+/usr/local/bin/wp plugin list > /www/locationsetbygps/install/wp-plugins.txt
